465a1066a0ea5fade3ce8d033bcfe231d6fd6f8f
syslao <syslao@gmail.com>
2016-05-23 17:50:04 +0300

    add acion cable for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,19 +24,18 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-
-
     @product = Product.new(product_params)
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
+        format.html { redirect_to @product,
+          notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created,
+          location: @product }
       else
         format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.json { render json: @product.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -46,11 +45,17 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to @product,
+          notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
+
+        @products = Product.all
+        ActionCable.server.broadcast 'products',
+          html: render_to_string('store/index', layout: false)
       else
         format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
+        format.json { render json: @product.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -60,19 +65,21 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.html { redirect_to products_url,
+          notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_product
-    @product = Product.find(params[:id])
-  end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def product_params
-    params.require(:product).permit(:title, :description, :image_url, :price)
-  end
+    # Never trust parameters from the scary internet, only allow the white
+    # list through.
+    def product_params
+      params.require(:product).permit(:title, :description, :image_url, :price)
+    end
 end
